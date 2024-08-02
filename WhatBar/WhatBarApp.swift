bdde8f63292f35c651912e30ab03a9059e23c408
Thomas Aardal <thomas_aardal@icloud.com>
2024-08-02 10:52:24 +0200

    Initial Commit

---
+++ WhatBar/WhatBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatBarApp.swift
+//  WhatBar
+//
+//  Created by Thomas Aardal on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
